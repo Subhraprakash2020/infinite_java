a797c46580cb174dcd21b4ec74723d4fe3eb8107
Subhraprakash Subudhi <67871717+Subhraprakash2020@users.noreply.github.com>
2023-09-15 00:52:54 +0530

    Delete Inventory_Project directory

--- Inventory_Project/src/main/java/com/java/inventory/OrderDAO.java
+++
@@ -1,7 +0,0 @@
-package com.java.inventory;
-
-import java.sql.SQLException;
-
-public interface OrderDAO {
-	String createOrder(String stockId, int QtyOrd) throws ClassNotFoundException, SQLException;
-}
